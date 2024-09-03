d22d8df2d1a9c20bd78f6ef9fe55289b34162814
OfirYC <ofir@yieldchain.io>
2024-09-03 13:52:04 +0300

    feat(trigger release): its good

--- src/pixianai.ts
+++ src/pixianai.ts
@@ -117,12 +117,3 @@ export class PixianAI {
     };
   }
 }
-
-function blobToB64(blob: Blob) {
-  return new Promise<string>((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onloadend = () => resolve(reader.result as string);
-    reader.onerror = reject;
-    reader.readAsDataURL(blob);
-  });
-}
