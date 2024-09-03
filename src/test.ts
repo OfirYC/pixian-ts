da9ed564a3d6d8352f248dc0d88da40ea54cb2cc
OfirYC <ofir@yieldchain.io>
2024-09-03 12:57:27 +0300

    feat(pixian client with removebackground): works with all file types

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,12 @@
 import * as fs from 'fs';
-import * as path from 'path';
-import { PixianAI } from '../src/pixianai';
+import { PixianAI } from './pixianai.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const filePath = path.join(__dirname, 'test.jpg');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, '../test.jpg');
 
 const apiId = process.env.PIXIAN_API_ID!;
 const apiSecret = process.env.PIXIAN_API_SECRET!;
@@ -14,39 +18,11 @@ const client = new PixianAI({
   apiId: apiId,
   apiSecret: apiSecret,
 });
-
-describe('index', () => {
-  describe('pixianclient', () => {
-    it('should remove the background from my image (Binary)', async () => {
-      const result = await client.removeBackground({
-        image: {
-          type: 'binary',
-          src: fs.createReadStream(filePath),
-        },
-        test: true,
-        maxPixels: 25000000,
-        result: {
-          cropToForeground: false,
-          margin: '0px',
-          verticalAlignment: 'middle',
-        },
-        output: {
-          format: 'auto',
-          jpegQuality: 75,
-        },
-      });
-
-      console.log(
-        'Result Ser!',
-        result.creditsCalculatedForTest,
-        result.creditsCharged,
-        result.imageB64.slice(0, 40)
-      );
-
-      return expect(result.creditsCharged).toEqual(0);
-    });
-  });
-});
+void (async () => {
+  void (await testBinaryFile());
+  void (await testB64File());
+  void (await testURLFile());
+})();
 
 async function testBinaryFile() {
   console.log(
@@ -72,23 +48,25 @@ async function testBinaryFile() {
 
 async function testB64File() {
   const fileBuffer = fs.readFileSync(filePath, { encoding: 'base64' });
-  await client.removeBackground({
-    image: {
-      type: 'b64',
-      src: fileBuffer,
-    },
-    test: true,
-    maxPixels: 25000000,
-    result: {
-      cropToForeground: false,
-      margin: '0px',
-      verticalAlignment: 'middle',
-    },
-    output: {
-      format: 'auto',
-      jpegQuality: 75,
-    },
-  });
+  console.log(
+    await client.removeBackground({
+      image: {
+        type: 'b64',
+        src: fileBuffer,
+      },
+      test: true,
+      maxPixels: 25000000,
+      result: {
+        cropToForeground: false,
+        margin: '0px',
+        verticalAlignment: 'middle',
+      },
+      output: {
+        format: 'auto',
+        jpegQuality: 75,
+      },
+    })
+  );
 }
 
 async function testURLFile() {
